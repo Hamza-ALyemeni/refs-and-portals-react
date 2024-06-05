72b9391c26c23a88044be61cd1c832715763dd78
Aloud <Aloud@tad.local>
2024-06-05 16:59:52 +0300

    using useImperativeHandle() hook for making ResultModel component more flexiable I guess .....

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -1,8 +1,18 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef} from "react";
 
 const ResultModel = forwardRef(function ResultModel({result,targetTime},ref){
+    const dialog = useRef();
+
+    useImperativeHandle(ref,()=>{
+        return{
+            open(){
+                dialog.current.showModal();
+            }
+        }
+    });
+
     return (
-    <dialog ref={ref} className="result-modal" >
+    <dialog ref={dialog} className="result-modal" >
         <h2>Your {result}</h2>
         <p>the target time was <strong>{targetTime} seconds.</strong></p>
         <p>you stopped the timer with <strong>X seconds left.</strong></p>
